0f12001e69fe355ec61271f992651e548e24682b
Gudupalli Aashrith <2300031300@kluniversity.in>
2026-04-15 19:23:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
+
+  const btn = document.getElementById("themeBtn");
+
+  if (document.body.classList.contains("dark")) {
+    btn.textContent = "☀️";
+  } else {
+    btn.textContent = "🌙";
+  }
 }
+
+const text = "I am a Web Developer";
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+typeEffect();
